9c9ceab122dacc2d75b95f5108b4f8dc21adfc23
tone4ka <toniamik@gmail.com>
2021-12-02 21:49:55 +0300

    feat: first route implementetion

--- src/fastifyApp.js
+++ src/fastifyApp.js
@@ -1,48 +1,33 @@
 // Require the framework and instantiate it
-// const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify')({ logger: true });
 // __________________________________________________________________________
-// // Declare a route
-// fastify.get('/', async (request, reply) => {
-//   return { hello: 'world' }
-// })
-
-// // Run the server!
-// const start = async () => {
-//   try {
-//     await fastify.listen(3000)
-//   } catch (err) {
-//     fastify.log.error(err)
-//     process.exit(1)
-//   }
-// }
-// start()
-// __________________________________________________________________________
-// fastify.route({
-//   method: 'GET',
-//   url: '/',
-//   schema: {
-//     // request needs to have a querystring with a `name` parameter
-//     querystring: {
-//       name: { type: 'string' },
-//     },
-//     // the response needs to be an object with an `hello` property of type 'string'
-//     response: {
-//       200: {
-//         type: 'object',
-//         properties: {
-//           hello: { type: 'string' },
-//         },
-//       },
-//     },
-//   },
-//   // this function is executed for every request before the handler is executed
-//   preHandler: async (request, reply) => {
-//     // E.g. check authentication
-//   },
-//   handler: async (request, reply) => {
-//     return { hello: 'world' };
-//   },
-// });
+fastify.route({
+  method: 'POST',
+  url: '/',
+  //   schema: {
+  //     // request needs to have a querystring with a `name` parameter
+  //     querystring: {
+  //       name: { type: 'string' },
+  //     },
+  //     // the response needs to be an object with an `hello` property of type 'string'
+  //     response: {
+  //       200: {
+  //         type: 'object',
+  //         properties: {
+  //           hello: { type: 'string' },
+  //         },
+  //       },
+  //     },
+  //   },
+  handler: async (request, reply) => {
+    console.log(request.body);
+    setImmediate(() => {
+      reply.send({ hello: request.body.name });
+    });
+    await reply;
+    // return { hello: request.body.name };
+  },
+});
 // __________________________________________________________________________
 //   fastify.register(require('./routes/events'), { prefix: '/' })
 
@@ -65,4 +50,35 @@
 
 //     done()
 //   }, { prefix: '/v1' })
+
+//   export const userRoutes = (fastify, options, done) => {
+//     fastify.get('/users', async (req, res) => {
+//       const users = await getAll();
+//       res.send(users.map(User.toResponse));
+//     });
+
+//     done();
+//   };
+
+// fastify.register(function (instance, opts, done) {
+//     instance.get('/foo', function (request, reply) {
+//       // Will log "prefix: /v1"
+//       request.log.info('prefix: %s', instance.prefix)
+//       reply.send({ prefix: instance.prefix })
+//     })
+
+//     instance.register(function (instance, opts, done) {
+//       instance.get('/bar', function (request, reply) {
+//         // Will log "prefix: /v1/v2"
+//         request.log.info('prefix: %s', instance.prefix)
+//         reply.send({ prefix: instance.prefix })
+//       })
+
+//       done()
+//     }, { prefix: '/v2' })
+
+//     done()
+//   }, { prefix: '/v1' })
 // __________________________________________________________________________
+
+module.exports = fastify;

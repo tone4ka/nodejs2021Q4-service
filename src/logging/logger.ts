06aad36dabd07e8a97a3bab110ba60ee0e3e650d
tone4ka <toniamik@gmail.com>
2021-12-23 23:26:30 +0300

    feat: start logging task

---
+++ src/logging/logger.ts
@@ -0,0 +1,17 @@
+class Logger {
+    // req: string;
+  
+    // title: string | undefined;
+  
+    // columns: Column[];
+  
+    constructor({
+    //   id = uuidv4(),
+    //   title = 'title',
+    //   columns = [new Column()]
+    } = {}) {
+    //   this.id = id;
+    //   this.title = title;
+    //   this.columns = columns.map((column) => new Column(column));
+    }
+}

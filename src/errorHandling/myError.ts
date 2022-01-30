d05d0a841ed9d2bf20eaf2115030c113e6311652
Antonina Zinkevich <antonina_zinkevich@epam.com>
2022-01-30 21:54:25 +0300

    seven failed tests

--- src/errorHandling/myError.ts
+++
@@ -1,9 +0,0 @@
-class MyError extends Error {
-    constructor(msg: string, replyStatusCode: number) {
-      super(msg);
-      this.message = msg;
-      this.name = `${replyStatusCode}`;
-    }
-  }
-
-export default MyError;
